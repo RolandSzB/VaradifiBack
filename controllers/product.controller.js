8260c59137f23f58183f16945b31cc6c8ac9b5e5
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2024-06-05 10:52:44 +0300

    contrller and routes added to product

---
+++ controllers/product.controller.js
@@ -0,0 +1,47 @@
+import {
+  createProduct,
+  getAllProducts,
+  deleteOneProduct,
+} from "../services/product.services";
+
+export async function getProducts(req, res) {
+  const dbProducts = await getAllProducts();
+  res.send(JSON.stringify(dbProducts));
+}
+
+export async function addNewProduct(req, res) {
+  //LUAREA DATELOR
+  const { name, tel, email, size, type } = req.body;
+  //VERIFICARI
+  if (!name) {
+    throw new Error("Name is required");
+  }
+  if (!tel) {
+    throw new Error("Telephone Number is required");
+  }
+  if (!email) {
+    throw new Error("Email is required");
+  }
+  if (!size) {
+    throw new Error("Size is required");
+  }
+  if (!type) {
+    throw new Error("Type is required");
+  }
+  //LOGICA - SERVICE + REPOSITORY
+  const productId = await createProduct(name, tel, email, size, type);
+  //RASPUNS
+  res.send(JSON.stringify({ id: productId }));
+}
+
+export async function deleteProduct(req, res) {
+  const { productId } = req.body;
+
+  if (!productId) {
+    throw new Error("Product id is required");
+  }
+
+  await deleteOneProduct(productId);
+
+  res.send("Deleted");
+}
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,26 +1,17 @@
 import { Router } from "express";
+
+import {
+  getProducts,
+  addNewProduct,
+  deleteProduct,
+} from "../controllers/product.controller";
+
 const router = Router();
 
-const products = [];
+router.get("/", getProducts);
 
-router.get("/", function (req, res) {
-  res.send(JSON.stringify(products));
-});
+router.post("/", addNewProduct);
 
-router.post("/", (req, res) => {
-  products.push(req.body);
-  console.log("PRO", products);
-  res.send("OK");
-});
-
-router.delete("/", (req, res) => {
-  const { productId } = req.body;
-
-  const productIndex = products.findIndex(
-    (product) => product.id === productId
-  );
-  products.splice(productIndex, 1);
-  res.send("Deleted product from database");
-});
+router.delete("/", deleteProduct);
 
 export default router;
